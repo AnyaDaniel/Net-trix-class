d233082dc45160af33f315cf41c19e682a7a5bc4
Mikeyboomin <100133629+Mikeyboomin@users.noreply.github.com>
2022-02-22 04:20:09 -0800

    Add files via upload

---
+++ Mikey.py
@@ -0,0 +1,8 @@
+myFullName = "Ogar Michael"
+phoneNum = "08152563202"
+Myemail = "info.mikeyboomin@gmail.com"
+Mylang = "python"
+MyPath = "frontend or Ui/Ux and Bankend"
+
+profile = " HELLO, My Name is " + myFullName + " My contact info is " + phoneNum + " and " + Myemail + " I'm using " + Mylang + " as the language for my " + MyPath
+print(profile)
