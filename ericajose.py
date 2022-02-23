4a2abbed4bfcf9034472357d8f2ea8d35ef34e41
ericajose <99970581+ericajose@users.noreply.github.com>
2022-02-23 09:59:05 +0100

    Add files via upload

---
+++ ericajose.py
@@ -0,0 +1,8 @@
+name = "vera abuo"
+phonenumber = "08161222745"
+email = "veraabuo2@gmail.com"
+address = "kado"
+language = "python"
+
+personalInfo = name + " " + phonenumber + " " + email + " " + address + " " + language
+print(personalInfo)
